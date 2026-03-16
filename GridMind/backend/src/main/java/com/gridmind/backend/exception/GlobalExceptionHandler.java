20010b370b20a03c16b26fd7dc3238b28239348c
CV17d <cristian.velasco@campusucc.edu.co>
2026-03-16 11:50:47 -0500

    Add global exception handler

---
+++ GridMind/backend/src/main/java/com/gridmind/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.gridmind.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiErrorResponse> handleRuntimeException(RuntimeException ex) {
+
+        ApiErrorResponse error = new ApiErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
